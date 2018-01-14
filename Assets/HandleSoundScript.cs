a3f8dd643229d6ca252c79d7a27fd3e40f49c540
AceBreaker <dws24520@yahoo.com>
2018-01-13 23:16:19 -0500

    Extra readme commit

---
+++ Assets/HandleSoundScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleSoundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(412, 732, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlaySound(AudioSource audio)
+    {
+        if (!audio.isPlaying)
+            audio.Play();
+        else
+            audio.Stop();
+    }
+}
